068c6306d739fd6bd6534c52327f76e2b511cb8d
Yurii Kichalo <yuriykichalo@gmail.com>
2021-01-09 13:38:42 +0200

    added base navigation

--- src/presentation/App.tsx
+++ src/presentation/App.tsx
@@ -1,6 +1,19 @@
 import React from 'react'
-import { Home } from 'presentation/screens'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import * as Screens from 'presentation/screens'
+import { AppRoutes } from './AppRoutes'
+import { RootStackParamsList } from './RootStackParamsList'
+
+const Stack = createStackNavigator<RootStackParamsList>()
 
 export const App = () => (
-  <Home />
+  <NavigationContainer>
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen
+        name={AppRoutes.Home}
+        component={Screens.Home}
+      />
+    </Stack.Navigator>
+  </NavigationContainer>
 )
